db53bcf54d4fa934f34cb9671e34128f0c600301
twixour <twixour@gmail.com>
2023-07-04 13:01:17 +0530

    Initial Commit

---
+++ FitnessMe/FitnessMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessMeApp.swift
+//  FitnessMe
+//
+//  Created by Raushan Kashyap on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
