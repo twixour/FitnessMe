f585a0790c2c86cc80c8d834fc3e091aafc949ea
twixour <twixour@gmail.com>
2023-07-04 13:19:52 +0530

    tab view gets tabitem removed as it is a page based app

--- FitnessMe/Views/ContentView.swift
+++ FitnessMe/Views/ContentView.swift
@@ -11,12 +11,9 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Text("Welcome")
-                .tabItem{Text("Welcome")}
-                
             Text("Exercise 1")
-                .tabItem{Text("Exercise 1")}
             Text("Exercise 2")
-                .tabItem{Text("Exercise 2")}
+                
         }
         .tabViewStyle(PageTabViewStyle())
         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
---
+++ FitnessMe/Views/ExerciseView.swift
@@ -0,0 +1,20 @@
+//
+//  ExerciseView.swift
+//  FitnessMe
+//
+//  Created by Raushan Kashyap on 04/07/23.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView()
+    }
+}
