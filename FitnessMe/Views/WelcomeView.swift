5ae74ea7f8a9902d05b5577e9561b6b518415bcc
twixour <twixour@gmail.com>
2023-07-05 10:51:36 +0530

    "Chapter - 4 In WelcomeView we added a new VStack for the centered content (other stack contains the HeaderView and History button) "

--- FitnessMe/Views/WelcomeView.swift
+++ FitnessMe/Views/WelcomeView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        HeaderView(titleText: "Welcome")
+        ZStack {
+            VStack {
+                HeaderView(titleText: "Welcome")
+                Spacer()
+                Button("History") {}
+                    .padding(.bottom)
+            }
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Get fit")
+                            .font(.largeTitle)
+                        Text("with high intensity interval training")
+                            .font(.headline)
+                    }
+                }
+            }
+        }
     }
 }
 
