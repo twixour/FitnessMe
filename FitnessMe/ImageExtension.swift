e0498c897b234ca77940bb8353300e12e76d2603
twixour <twixour@gmail.com>
2023-07-05 11:16:08 +0530

    "Chapter - 4 Added Date Extension and ImageExtension"

---
+++ FitnessMe/ImageExtension.swift
@@ -0,0 +1,22 @@
+//
+//  ImageExtension.swift
+//  FitnessMe
+//
+//  Created by Raushan Kashyap on 05/07/23.
+//
+
+import SwiftUI
+
+extension Image {
+  /// Resize an image with fill aspect ratio and specified frame dimensions.
+  ///   - parameters:
+  ///     - width: Frame width.
+  ///     - height: Frame height.
+  func resizedToFill(width: CGFloat, height: CGFloat)
+  -> some View {
+    self
+      .resizable()
+      .aspectRatio(contentMode: .fill)
+      .frame(width: width, height: height)
+  }
+}
